69d0efc24f9e6bcd4e48f0fdf8ab7ed84832c8bf
myyrakle <sssang97@naver.com>
2020-10-01 01:09:36 +0900

    add blue, green, yellow

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const chalk = require("chalk");
 
-console.red = (...args) => {
-    console.log(chalk.red(...args));
-};
+console.red = (...args) => console.log(chalk.red(chalk.bold(...args)));
+console.blue = (...args) => console.log(chalk.blue(chalk.bold(...args)));
+console.green = (...args) => console.log(chalk.green(chalk.bold(...args)));
+console.yellow = (...args) => console.log(chalk.yellow(chalk.bold(...args)));
 
 module.exports = {};
