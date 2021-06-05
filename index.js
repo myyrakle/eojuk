9b92ea24283c213c6b8a1697521825ca62a7a971
myyrakle <sssang97@naver.com>
2021-06-06 02:03:29 +0900

    프로젝트 변경(sql ddl->스키마 변환기)

--- index.js
+++
@@ -1,8 +0,0 @@
-const chalk = require("chalk");
-
-console.red = (...args) => console.log(chalk.red(chalk.bold(...args)));
-console.blue = (...args) => console.log(chalk.blue(chalk.bold(...args)));
-console.green = (...args) => console.log(chalk.green(chalk.bold(...args)));
-console.yellow = (...args) => console.log(chalk.yellow(chalk.bold(...args)));
-
-module.exports = {};
