5f6d8eae17c61c9fde4d22dc1dd3f056c2af657e
myyrakle <sssang97@naver.com>
2021-07-18 03:03:35 +0900

    테스트 케이스 추가

---
+++ test/mysql/test2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `action` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `platform` enum('IOS','ANDROID','PC_WEB','MOBILE_WEB','ETC') NOT NULL DEFAULT 'PC_WEB',
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_11db75ea5697b4c806aedc0739` (`type`),
+  KEY `IDX_2820136a62f5ae380b8d76f9dc` (`itemId`),
+  KEY `IDX_51c78f7dbdc63cdfb816e74aab` (`date`)
+);
