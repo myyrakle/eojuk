c3a29eb633f2bcc6158e2fca651640fc21f5c3ec
myyrakle <sssang97@naver.com>
2023-04-28 19:49:01 +0900

    fix: jpa gen

--- src/output/jpa-java.ts
+++ src/output/jpa-java.ts
@@ -4,9 +4,9 @@ import { IEmmiter } from "../interfaces/emitter";
 import Source from "../types/source";
 import Table from "../types/table";
 import {
-    convertNameCaseByOption,
-    toCamelCase,
-    toSnakeCase,
+  convertNameCaseByOption,
+  toCamelCase,
+  toSnakeCase,
 } from "../util.ts/name";
 import { TAB } from "../util.ts/tab";
 import { escapeDoubleQuote } from "../util.ts/escape";
@@ -22,97 +22,101 @@ import java.time.LocalDateTime;
 `;
 
 export class JPAEmitter implements IEmmiter {
-    private option: IOption;
+  private option: IOption;
 
-    // 컬럼 필드 코드 생성
-    private generateColumn(column: Column) {
-        const columnFieldName = convertNameCaseByOption(
-            this.option.outputFieldNameCase,
-            column.name
-        );
+  // 컬럼 필드 코드 생성
+  private generateColumn(column: Column) {
+    const columnFieldName = convertNameCaseByOption(
+      this.option.outputFieldNameCase,
+      column.name
+    );
 
-        const hasCreatedAt = column.name == this.option.autoAddCreatedAt
-        const hasUpdatedAt = column.name == this.option.autoAddUpdatedAt
-        // const hasDeletedAt = column.name == this.option.autoAddDeletedAt
+    const hasCreatedAt = column.name == this.option.autoAddCreatedAt;
+    const hasUpdatedAt = column.name == this.option.autoAddUpdatedAt;
+    // const hasDeletedAt = column.name == this.option.autoAddDeletedAt
 
-        // PrimaryKey 강제 추가 옵션
-        if(column.name == this.option.autoAddPrimaryKey) {
-            column.isPrimaryKey = true;
-        }
+    // PrimaryKey 강제 추가 옵션
+    if (column.name == this.option.autoAddPrimaryKey) {
+      column.isPrimaryKey = true;
+    }
 
-        const createdAt = hasCreatedAt ? `\n${TAB}@CreationTimestamp` : "";
-        const updatedAt = hasUpdatedAt ? `\n${TAB}@UpdateTimestamp` : "";
-        const deletedAt = ""; // hibernate에는 해당 기능이 없음
+    const createdAt = hasCreatedAt ? `\n${TAB}@CreationTimestamp` : "";
+    const updatedAt = hasUpdatedAt ? `\n${TAB}@UpdateTimestamp` : "";
+    const deletedAt = ""; // hibernate에는 해당 기능이 없음
 
-        const primaryKey = column.isPrimaryKey
-            ? `\n${TAB}Id`
-            : "";
+    const primaryKey = column.isPrimaryKey ? `\n${TAB}@Id` : "";
 
-        const autoIncrement = column.isAutoIncrement
-            ? `\n${TAB}@GeneratedValue(strategy = GenerationType.IDENTITY)`
-            : "";
+    const autoIncrement = column.isAutoIncrement
+      ? `\n${TAB}@GeneratedValue(strategy = GenerationType.IDENTITY)`
+      : "";
 
-        const defaultValue = column.default
-            ? `\n${TAB}@ColumnDefault("${escapeDoubleQuote(column.default)}")`
-            : "";
+    const defaultValue = column.default
+      ? `\n${TAB}@ColumnDefault("${escapeDoubleQuote(column.default)}")`
+      : "";
 
-        const comment = column.comment ?  `\n${TAB}@Comment("${escapeDoubleQuote(column.comment)}")` : ''
+    const comment = column.comment
+      ? `\n${TAB}@Comment("${escapeDoubleQuote(column.comment)}")`
+      : "";
 
-        //const dataType = this.dbTypeToDataType(column.dbType);
+    //const dataType = this.dbTypeToDataType(column.dbType);
 
-        const columnAnnotation = `\n${TAB}@Column(name = "${escapeDoubleQuote(column.name)}")`
+    const columnAnnotation = `\n${TAB}@Column(name = "${escapeDoubleQuote(
+      column.name
+    )}")`;
 
-        const notNullAnnotaion = column.isNotNull ? `\n${TAB}@NotNull` : `\n${TAB}@Nullable`
+    const notNullAnnotaion = column.isNotNull
+      ? `\n${TAB}@Nonnull`
+      : `\n${TAB}@Nullable`;
 
-        return `${primaryKey}${autoIncrement}${columnAnnotation}${notNullAnnotaion}${comment}${defaultValue}${createdAt}${updatedAt}${deletedAt}
+    return `${primaryKey}${autoIncrement}${columnAnnotation}${notNullAnnotaion}${comment}${defaultValue}${createdAt}${updatedAt}${deletedAt}
 ${TAB}${column.javaType} ${columnFieldName};`;
-    }
+  }
 
-    // 테이블 클래스 코드 생성
-    private generateTableCode(table: Table) {
-        const hasDatabaseName = this.option.databaseName != null
+  // 테이블 클래스 코드 생성
+  private generateTableCode(table: Table) {
+    const hasDatabaseName = this.option.databaseName != null;
 
-        const tableClassName = convertNameCaseByOption(
-            this.option.outputClassNameCase,
-            table.tableName
-        );
+    const tableClassName = convertNameCaseByOption(
+      this.option.outputClassNameCase,
+      table.tableName
+    );
 
-        const schema = hasDatabaseName ? `, schema = "\"${this.option.databaseName}\""` : "";
+    const schema = hasDatabaseName
+      ? `, schema = "\\"${this.option.databaseName}\\""`
+      : "";
 
-        return `@Entity()
-@Table(name = "\"${table.tableName}\""${schema})
+    return `@Entity()
+@Table(name = "\\"${table.tableName}\\""${schema})
 public class ${tableClassName} {
-${table.columns.map((column) => this.generateColumn(column)).join("\n\n")}
+${table.columns.map((column) => this.generateColumn(column)).join("\n")}
 }`;
-    }
+  }
 
-    emit(
-        tables: Table[],
-        option: IOption = {
-            sourceSplit: true,
-            outputClassNameCase: "PASCAL",
-            outputFieldNameCase: "CAMEL",
-        }
-    ): Source[] {
-        this.option = option;
-
-        if (option?.sourceSplit) {
-            return tables.map((table) => ({
-                sourceName: table.tableName,
-                source: importTemplate + "\n" + this.generateTableCode(table),
-            }));
-        } else {
-            return [
-                {
-                    sourceName: "all",
-                    source:
-                        importTemplate +
-                        "\n" +
-                        tables
-                            .map((table) => this.generateTableCode(table))
-                            .join("\n\n"),
-                },
-            ];
-        }
+  emit(
+    tables: Table[],
+    option: IOption = {
+      sourceSplit: true,
+      outputClassNameCase: "PASCAL",
+      outputFieldNameCase: "CAMEL",
     }
+  ): Source[] {
+    this.option = option;
+
+    if (option?.sourceSplit) {
+      return tables.map((table) => ({
+        sourceName: table.tableName,
+        source: importTemplate + "\n" + this.generateTableCode(table),
+      }));
+    } else {
+      return [
+        {
+          sourceName: "all",
+          source:
+            importTemplate +
+            "\n" +
+            tables.map((table) => this.generateTableCode(table)).join("\n\n"),
+        },
+      ];
+    }
+  }
 }
