009a57a86e575e06807b3f8f400895042f04c98e
myyrakle <sssang97@naver.com>
2023-10-18 22:56:54 +0900

    [#13] complete

--- test/pg/test_for_go.sql
+++ test/pg/test_for_go.sql
@@ -2,30 +2,25 @@ CREATE TABLE "trade_history" (
 	"_id"	ObjectID		NOT NULL,
 	"sellerID"	int		NOT NULL,
 	"buyerID"	int		NOT NULL,
-	"sell_offer_id"	int		NOT NULL,
-	"buy_offer_id"	int		NOT NULL,
-	"created_at"	Time	NOT 	NULL,
-	"updated_at"	Time  	NOT NULL,
-	"deleted_at"	Time		NOT NULL
+	"sellOfferID"	int		NOT NULL,
+	"buyOfferID"	int		NOT NULL,
+	"createDate"	Time	 	NULL,
+	"updateDate"	Time  	NOT NULL,
+	"deleteDate"	Time		NOT NULL
 );
 
-COMMENT ON COLUMN "nft_trade_history"."id" IS '기본키';
+COMMENT ON COLUMN "trade_history"."_id" IS '기본키';
 
-COMMENT ON COLUMN "nft_trade_history"."seller_id" IS '판매자 ID';
+COMMENT ON COLUMN "trade_history"."sellerID" IS '판매자 ID';
 
-COMMENT ON COLUMN "nft_trade_history"."buyer_id" IS '구매자 ID';
+COMMENT ON COLUMN "trade_history"."buyerID" IS '구매자 ID';
 
-COMMENT ON COLUMN "nft_trade_history"."sell_offer_id" IS '판매 등록';
+COMMENT ON COLUMN "trade_history"."sellOfferID" IS '판매 등록';
 
-COMMENT ON COLUMN "nft_trade_history"."buy_offer_id" IS '구매 요청';
+COMMENT ON COLUMN "trade_history"."buyOfferID" IS '구매 요청';
 
-COMMENT ON COLUMN "nft_trade_history"."created_at" IS '생성일자';
+COMMENT ON COLUMN "trade_history"."createDate" IS '생성일자';
 
-COMMENT ON COLUMN "nft_trade_history"."updated_at" IS '수정일자';
+COMMENT ON COLUMN "trade_history"."updateDate" IS '수정일자';
 
-COMMENT ON COLUMN "nft_trade_history"."deleted_at" IS '삭제일자';
-
-ALTER TABLE "trade_history" ADD CONSTRAINT "PK_NFT_TRADE_HISTORY" PRIMARY KEY (
-	"id"
-);
-
+COMMENT ON COLUMN "trade_history"."deleteDate" IS '삭제일자';
